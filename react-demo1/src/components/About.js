2b4bb9e8be5ea8b185e327edab870259e03ab2be
David Lopez Felix <dlopezf.19@gmail.com>
2024-04-08 20:44:35 -0500

    Adding my name

--- react-demo1/src/components/About.js
+++ react-demo1/src/components/About.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 const About = () => {
   return (
     <div>
-      <h1>About Page</h1>
+      <h1>Restricted David's Page</h1>
       <p>Learn more about us...</p>
       <Link to="/">Go to Home Page</Link>
     </div>
--- react-demo1/src/components/Home.js
+++ react-demo1/src/components/Home.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 const Home = () => {
   return (
     <div>
-      <h1>Home Page</h1>
+      <h1>Public David's Page</h1>
       <p>Welcome to the Home Page</p>
       <Link to="/about">Go to About Page</Link>
     </div>
